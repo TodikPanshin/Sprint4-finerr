cde4290b260b6151fac2f72735fb2e22b0b94dca
yonipels <pels.yona@mail.huji.ac.il>
2023-06-01 18:06:28 +0300

    update css

--- frontend/src/cmps/gig-preview.jsx
+++ frontend/src/cmps/gig-preview.jsx
@@ -4,17 +4,19 @@ export function GigPreview({ gig }) {
     return <article className="gig-preview flex column">
         <Link to={`/gig/${gig._id}`} target="_blank" >
             <div className="img-preview">
-                <img src={gig.imgUrl} alt="gig img" />
+                <img src={gig.imgUrl} alt="gig img" className="gig-img" />
             </div>
         </Link>
         <div className="owner-details flex">
-            <img src={gig.owner.imgUrl} alt="img of creator" />
+            <img src={gig.owner.imgUrl} alt="img of creator" className="owner-img" />
             <Link to={`/user/${gig.owner._id}`} className="owner-name">  {gig.owner.fullname}</Link>
         </div>
         <Link to={`/gig/${gig._id}`} target="_blank"><h3 className="title">{gig.title}</h3></Link>
 
         {/* <p>From <span>${gig.price.toLocaleString()}</span></p> */}
-        <div className="rating flex">*{gig.rating.average} <span>({gig.rating.num})</span> </div>
+        <div className="rating flex">
+            {/* <FontAwesomeIcon icon="fa-solid fa-star" /> */}
+            {gig.rating.average} <span>({gig.rating.num})</span> </div>
         <Link to={`/gig/${gig._id}`} target="_blank"><p className="price txt-bold">From <span>${gig.price}</span></p></Link>
     </article>
 }
