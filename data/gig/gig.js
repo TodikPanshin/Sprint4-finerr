6ef59a2249c981904b0dd0f26c6baff8b21f5def
yonipels <pels.yona@mail.huji.ac.il>
2023-06-01 20:46:30 +0300

    update ui

--- data/gig/gig.js
+++ data/gig/gig.js
@@ -26,6 +26,273 @@ const gig = {
   "likedByUsers": ["mini-user"]
 }
 
+const gigs = [
+  {
+    "_id": "i101",
+    "title": "I will design your logo",
+    "price": 12,
+    "rating": {
+      "average": 5,
+      "num": 178
+    },
+    "owner": {
+      "_id": "u101",
+      "fullname": "Dudu Da",
+      "imgUrl": "url",
+      "level": "basic/premium",
+      "rate": 4
+    },
+    "daysToMake": 3,
+    "description": "Make unique logo...",
+    "imgUrl": "https://picsum.photos/200/300",
+    "tags": [
+      "logo-design",
+      "artisitic",
+      "proffesional",
+      "accessible"
+    ],
+    "likedByUsers": ["mini-user"]
+  },
+  {
+    "_id": "i102",
+    "title": "I will write your blog post",
+    "price": 20,
+    "rating": {
+      "average": 4.5,
+      "num": 150
+    },
+    "owner": {
+      "_id": "u102",
+      "fullname": "Ava Ava",
+      "imgUrl": "url",
+      "level": "basic/premium",
+      "rate": 5
+    },
+    "daysToMake": 5,
+    "description": "Write informative and engaging blog posts...",
+    "imgUrl": "https://picsum.photos/200/300",
+    "tags": [
+      "blog-writing",
+      "informative",
+      "engaging",
+      "professional"
+    ],
+    "likedByUsers": ["mini-user"]
+  },
+  {
+    "_id": "i103",
+    "title": "I will create your website",
+    "price": 50,
+    "rating": {
+      "average": 4,
+      "num": 100
+    },
+    "owner": {
+      "_id": "u103",
+      "fullname": "Kiki Kiki",
+      "imgUrl": "url",
+      "level": "basic/premium",
+      "rate": 3
+    },
+    "daysToMake": 10,
+    "description": "Create a beautiful and responsive website...",
+    "imgUrl": "https://picsum.photos/200/300",
+    "tags": [
+      "website-design",
+      "beautiful",
+      "responsive",
+      "professional"
+    ],
+    "likedByUsers": ["mini-user"]
+  },
+  {
+    "_id": "i104",
+    "title": "I will develop your app",
+    "price": 100,
+    "rating": {
+      "average": 3.5,
+      "num": 50
+    },
+    "owner": {
+      "_id": "u104",
+      "fullname": "Lulu Lulu",
+      "imgUrl": "url",
+      "level": "basic/premium",
+      "rate": 2
+    },
+    "daysToMake": 15,
+    "description": "Develop a functional and user-friendly app...",
+    "imgUrl": "https://picsum.photos/200/300",
+    "tags": [
+      "app-development",
+      "functional",
+      "user-friendly",
+      "professional"
+    ],
+    "likedByUsers": ["mini-user"]
+  },
+  {
+    "_id": "i105",
+    "title": "I will write your ad copy",
+    "price": 15,
+    "rating": {
+      "average": 4.5,
+      "num": 100
+    },
+    "owner": {
+      "_id": "u105",
+      "fullname": "Mia Mia",
+      "imgUrl": "url",
+      "level": "basic/premium",
+      "rate": 5
+    },
+    "daysToMake": 2,
+    "description": "I will write your ad copy",
+    "imgUrl": "https://picsum.photos/200/300",
+    "tags": [
+      "app-development",
+      "functional",
+      "user-friendly",
+      "professional"
+    ],
+    "likedByUsers": ["mini-user"]
+  },
+  {
+    "_id": "i106",
+    "title": "I will create your marketing materials",
+    "price": 30,
+    "rating": {
+      "rating": {
+        "average": 4.5,
+        "num": 100
+  },
+  "owner": {
+    "_id": "u106",
+    "fullname": "Ni Ni",
+    "imgUrl": "url",
+    "level": "basic/premium",
+    "rate": 3
+  },
+  "daysToMake": 5,
+  "description": "Create eye-catching and persuasive marketing materials...",
+  "imgUrl": "https://picsum.photos/200/300",
+  "tags": [
+    "marketing-materials",
+    "eye-catching",
+    "persuasive",
+    "professional"
+  ],
+  "likedByUsers": ["mini-user"]
+  }
+},
+{
+  "_id": "i107",
+    "title": "I will manage your social media",
+      "price": 50,
+        "rating": {
+    "average": 4.5,
+      "num": 100
+  },
+  "owner": {
+    "_id": "u107",
+      "fullname": "Oli Oli",
+        "imgUrl": "url",
+          "level": "basic/premium",
+            "rate": 5
+  },
+  "daysToMake": 10,
+    "description": "Grow your social media following and engagement...",
+      "imgUrl": "https://picsum.photos/200/300",
+        "tags": [
+          "social-media-management",
+          "grow-following",
+          "engagement",
+          "professional"
+        ],
+          "likedByUsers": ["mini-user", "mini-user2"]
+},
+{
+  "_id": "i108",
+    "title": "I will create your email marketing campaigns",
+      "price": 25,
+        "rating": {
+    "average": 4,
+      "num": 50
+  },
+  "owner": {
+    "_id": "u108",
+      "fullname": "Poppy Poppy",
+        "imgUrl": "url",
+          "level": "basic/premium",
+            "rate": 3
+  },
+  "daysToMake": 3,
+    "description": "Create effective email marketing campaigns that convert...",
+      "imgUrl": "https://picsum.photos/200/300",
+        "tags": [
+          "email-marketing",
+          "effective",
+          "convert",
+          "professional"
+        ],
+          "likedByUsers": ["mini-user"]
+},
+{
+  "_id": "i109",
+    "title": "I will help you with your SEO",
+      "price": 100,
+        "rating": {
+    "average": 3.5,
+      "num": 25
+  },
+  "owner": {
+    "_id": "u109",
+      "fullname": "Romy Romy",
+        "imgUrl": "url",
+          "level": "basic/premium",
+            "rate": 2
+  },
+  "daysToMake": 15,
+    "description": "Improve your website's search engine ranking...",
+      "imgUrl": "https://picsum.photos/200/300",
+        "tags": [
+          "seo",
+          "improve-ranking",
+          "professional"
+        ],
+          "likedByUsers": []
+},
+{
+  "_id": "i110",
+    "title": "I will create your video content",
+      "price": 50,
+        "rating": {
+    "average": 4.5,
+      "num": 100
+  },
+  "owner": {
+    "_id": "u110",
+      "fullname": "Suki Suki",
+        "imgUrl": "url",
+          "level": "basic/premium",
+            "rate": 5
+  },
+  "daysToMake": 10,
+    "description": "Create engaging and informative video content...",
+      "imgUrl": "https://picsum.photos/200/300",
+        "tags": [
+          "seo",
+          "improve-ranking",
+          "professional"
+        ],
+          "likedByUsers": []
+},
+]
+
+
+
+
+
 
 const orders = [
   {
@@ -44,13 +311,13 @@ const orders = [
 
 const users = [
   {
-    _id: "u101",
-    fullname: "User 1",
-    imgUrl: "/img/img1.jpg",
-    username: "user1",
-    password: "secret",
-    level: "basic/premium",
-    reviews: [
+    "_id": "u101",
+    "fullname": "User 1",
+    "imgUrl": "/img/img1.jpg",
+    "username": "user1",
+    "password": "secret",
+    "level": "basic/premium",
+    "reviews": [
       {
         "id": "madeId",
         "gig": "{optional-mini-gig}",
