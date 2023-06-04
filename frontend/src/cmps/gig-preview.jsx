8095bf748f9ebbe17d43909628042f483d2d9c44
yonipels <pels.yona@mail.huji.ac.il>
2023-06-04 16:46:42 +0300

    add star

--- frontend/src/cmps/gig-preview.jsx
+++ frontend/src/cmps/gig-preview.jsx
@@ -3,6 +3,15 @@ import { PreviewCarousel } from "./preview-carousel"
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(fas)
+
+
 export function GigPreview({ gig }) {
 
     return (
@@ -19,7 +28,13 @@ export function GigPreview({ gig }) {
             <Link to={`/gig/${gig._id}`} className="title">{gig.title}</Link>
 
             <div className="rating flex">
-                {gig.rating.average} <span>({gig.rating.num})</span>
+                <span className="star">
+                    <FontAwesomeIcon icon="fa-solid fa-star" />
+                </span>
+                <span className="average">
+                    {gig.rating.average}
+                </span>
+                <span className="num">({gig.rating.num})</span>
             </div>
 
             <Link to={`/gig/${gig._id}`} ><p className="price txt-bold">From <span>${gig.price}</span></p></Link>
