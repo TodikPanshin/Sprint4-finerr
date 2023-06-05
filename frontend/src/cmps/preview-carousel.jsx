adc26f3ddd7cb6e042e49f353cedc954fd5fbfd0
yonipels <pels.yona@mail.huji.ac.il>
2023-06-05 20:25:56 +0300

    preview carousel perfect pixel

--- frontend/src/cmps/preview-carousel.jsx
+++ frontend/src/cmps/preview-carousel.jsx
@@ -1,78 +1,3 @@
-// import React, { useState } from "react";
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-
-// library.add(fas);
-
-// export function PreviewCarousel({ gig }) {
-//     const [hoveredIndex, setHoveredIndex] = useState(null);
-
-//     const handleMouseEnter = (index) => {
-//         setHoveredIndex(index);
-//     };
-
-//     const handleMouseLeave = () => {
-//         setHoveredIndex(null);
-//     };
-
-//     const renderArrowPrev = (clickHandler, hasPrev, label) => (
-//         <div
-//             className={`custom-arrow custom-arrow-prev ${hoveredIndex === null ? "" : "active"
-//                 }`}
-//             onClick={clickHandler}
-//         >
-//             <FontAwesomeIcon
-//                 icon={faChevronLeft}
-//                 style={{
-//                     "--fa-primary-color": "#62646a",
-//                     "--fa-secondary-color": "#ffffff",
-//                 }}
-//             />
-//         </div>
-//     );
-
-//     const renderArrowNext = (clickHandler, hasNext, label) => (
-//         <div
-//             className={`custom-arrow custom-arrow-next ${hoveredIndex === null ? "" : "active"
-//                 }`}
-//             onClick={clickHandler}
-//         >
-//             <FontAwesomeIcon
-//                 icon={faChevronRight}
-//                 style={{
-//                     "--fa-primary-color": "#62646a",
-//                     "--fa-secondary-color": "#ffffff",
-//                 }}
-//             />
-//         </div>
-//     );
-
-//     return (
-//         <Carousel
-//             showThumbs={false}
-//             showStatus={false}
-//             className="preview-carousel"
-//             renderArrowPrev={renderArrowPrev}
-//             renderArrowNext={renderArrowNext}
-//         >
-//             {gig.imgUrls.map((imgUrl, index) => (
-//                 <div
-//                     key={imgUrl}
-//                     className="image-wrapper"
-//                     onMouseEnter={() => handleMouseEnter(index)}
-//                     onMouseLeave={handleMouseLeave}
-//                 >
-//                     <img alt="" src={imgUrl} />
-//                 </div>
-//             ))}
-//         </Carousel>
-//     );
-// }
-
 import React, { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -93,6 +18,27 @@ export function PreviewCarousel({ gig }) {
     // const handleMouseLeave = () => {
     //     setIsHovered(false);
     // }
+
+    const renderIndicator = (clickHandler, isSelected, index, label) => {
+        const indicatorClasses = isSelected ? "carousel-indicator selected-indicator" : "carousel-indicator";
+        const indicatorStyle = isSelected ? { transform: "scale(1.5)" } : {};
+
+        return (
+            <div
+                key={index}
+                className={indicatorClasses}
+                onClick={(ev) => {
+                    ev.preventDefault()
+                    clickHandler()
+                }}
+                onKeyDown={clickHandler}
+                role="button"
+                tabIndex={0}
+                aria-label={label}
+                style={indicatorStyle}
+            />
+        )
+    }
 
     const renderArrowPrev = (clickHandler, hasPrev, label) => (
         <div
@@ -116,10 +62,10 @@ export function PreviewCarousel({ gig }) {
         <div
             className={`custom-arrow custom-arrow-next`}
             onClick={(ev) => {
-                ev.preventDefault()
-                // ev.stopPropagation()
-                clickHandler()
-            }}        >
+                ev.preventDefault();
+                clickHandler();
+            }}
+        >
             <FontAwesomeIcon
                 icon={faChevronRight}
                 size="2xs"
@@ -135,9 +81,12 @@ export function PreviewCarousel({ gig }) {
         <Carousel
             showThumbs={false}
             showStatus={false}
-            className="preview-carousel"
+            infiniteLoop
             renderArrowPrev={renderArrowPrev}
             renderArrowNext={renderArrowNext}
+            renderIndicator={renderIndicator}
+            className="preview-carousel"
+
         // onMouseEnter={handleMouseEnter}
         // onMouseLeave={handleMouseLeave}
         >
