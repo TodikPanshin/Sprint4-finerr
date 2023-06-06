d7fe4a903ed6d80370660a4c71491f2d1faaa4b0
yonipels <pels.yona@mail.huji.ac.il>
2023-06-06 17:27:54 +0300

    update layout

--- frontend/src/cmps/gig-preview.jsx
+++ frontend/src/cmps/gig-preview.jsx
@@ -15,32 +15,39 @@ library.add(fas)
 export function GigPreview({ gig }) {
 
     return (
-        <article className="gig-preview flex column">
-            <Link to={`/gig/${gig._id}`} >
-                <div className="img-container">
-                    <PreviewCarousel gig={gig} className="gig-img" />
+        <>
+            {/* <FontAwesomeIcon icon="fa-regular fa-heart" size="2xs" style={{ color: "#565656", }} />
+            <FontAwesomeIcon icon="fa-solid fa-star" />
+            <FontAwesomeIcon icon="fa-regular fa-heart" style={{ color: "#511f29", }} /> */}
+
+            <article className="gig-preview flex column">
+                <Link to={`/gig/${gig._id}`} >
+                    <div className="img-container">
+                        <PreviewCarousel gig={gig} className="gig-img" />
+                        {/* <FontAwesomeIcon icon="fa-regular fa-heart" size="2xs" style={{ color: "#ffffff", }} /> */}
+                    </div>
+                </Link>
+
+                <div className="owner-details flex">
+                    <div className="owner-img flex">
+                        <img src={gig.owner.imgUrl} alt="img of owner" />
+                    </div>
+                    <Link to={`/user/${gig.owner._id}`} className="owner-name">  {gig.owner.fullname}</Link>
                 </div>
-            </Link>
 
-            <div className="owner-details flex">
-                <div className="owner-img flex">
-                    <img src={gig.owner.imgUrl} alt="img of owner" />
+                <Link to={`/gig/${gig._id}`} className="gig-title">{gig.title}</Link>
+
+                <div className="gig-rating flex">
+                    <span className="star">
+                        <FontAwesomeIcon icon="fa-solid fa-star" />
+                    </span>
+                    <span className="average">
+                        {gig.rating.average}
+                    </span>
+                    <span className="num">({gig.rating.num})</span>
                 </div>
-                <Link to={`/user/${gig.owner._id}`} className="owner-name">  {gig.owner.fullname}</Link>
-            </div>
 
-            <Link to={`/gig/${gig._id}`} className="title">{gig.title}</Link>
-
-            <div className="rating flex">
-                <span className="star">
-                    <FontAwesomeIcon icon="fa-solid fa-star" />
-                </span>
-                <span className="average">
-                    {gig.rating.average}
-                </span>
-                <span className="num">({gig.rating.num})</span>
-            </div>
-
-            <Link to={`/gig/${gig._id}`} ><p className="price txt-bold">From <span>${gig.price}</span></p></Link>
-        </article >)
+                <Link to={`/gig/${gig._id}`} ><p className="gig-price txt-bold">From <span>${gig.price}</span></p></Link>
+            </article >
+        </>)
 }
