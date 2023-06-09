55883b77b032044d9e0448e56e244394c21c4766
todik panshin <todikp@gmail,com>
2023-06-09 14:13:29 +0300

    add Checkout

--- frontend/src/pages/checkout.jsx
+++ frontend/src/pages/checkout.jsx
@@ -1,3 +1,81 @@
+// export function Checkout() {
+//     return (
+//     <h1>Checkout</h1>
+//     )
+// }
+
+import React, { useState } from "react";
+
 export function Checkout() {
-    return <h1>Checkout</h1>
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  const handleCardNumberChange = (e) => {
+    setCardNumber(e.target.value);
+  };
+
+  const handleExpiryDateChange = (e) => {
+    setExpiryDate(e.target.value);
+  };
+
+  const handleCvvChange = (e) => {
+    setCvv(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    // For example, make an API request to process the payment
+    console.log("Card Number:", cardNumber);
+    console.log("Expiry Date:", expiryDate);
+    console.log("CVV:", cvv);
+    // Reset form fields
+    setCardNumber("");
+    setExpiryDate("");
+    setCvv("");
+  };
+
+  return (
+    <div className="payment-container">
+      <h2>Payment Options</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="cardNumber">Card Number</label>
+          <input
+            type="text"
+            id="cardNumber"
+            value={cardNumber}
+            onChange={handleCardNumberChange}
+            placeholder="Enter your card number"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="expiryDate">Expiry Date</label>
+          <input
+            type="text"
+            id="expiryDate"
+            value={expiryDate}
+            onChange={handleExpiryDateChange}
+            placeholder="MM/YY"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="cvv">CVV</label>
+          <input
+            type="text"
+            id="cvv"
+            value={cvv}
+            onChange={handleCvvChange}
+            placeholder="Enter CVV"
+            required
+          />
+        </div>
+        <button type="submit">Pay Now</button>
+      </form>
+    </div>
+  )
 }
+
