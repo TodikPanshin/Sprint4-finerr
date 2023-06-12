380386d8508a2702bdc544465c45b940002173f9
NadavEliash <nadav.eliash@gmail.com>
2023-06-12 17:06:01 +0300

    update pacjage

--- backend/config/index.mjs
+++ backend/config/index.mjs
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log("prod", configProd)
   config = configProd
 } else {
+  console.log('hi!');
   config = configDev
 }
 config.isGuestMode = true
