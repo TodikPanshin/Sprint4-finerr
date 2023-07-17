75520923fe8d780ce68a089968c26e66d85b6e00
yoni pels <yona.pels@gmail.com>
2023-07-17 16:31:20 +0300

    responsive categories

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -89,6 +89,37 @@ export function HomePage() {
         setCarouselStyle(prevStyle => ({ ...prevStyle, right: `${carouselRight.current}rem` }))
     }
 
+    
+    function handleSwipe(event) {
+        const touchStartX = event.touches[0].clientX;
+        let isSwiping = false;
+      
+        function handleTouchMove(e) {
+          if (!isSwiping) {
+            const touchMoveX = e.touches[0].clientX;
+            const diffX = touchStartX - touchMoveX;
+            const SWIPE_THRESHOLD = 10;
+      
+            if (diffX > SWIPE_THRESHOLD) {
+              slideCarousel(1); // Move to the next slide
+              isSwiping = true;
+            } else if (diffX < -SWIPE_THRESHOLD) {
+              slideCarousel(-1); // Move to the previous slide
+              isSwiping = true;
+            }
+          }
+        }
+      
+        function handleTouchEnd() {
+          isSwiping = false;
+          document.removeEventListener('touchmove', handleTouchMove);
+          document.removeEventListener('touchend', handleTouchEnd);
+        }
+      
+        document.addEventListener('touchmove', handleTouchMove);
+        document.addEventListener('touchend', handleTouchEnd);
+      }
+
     return (
         <main className="home-page main-layout full">
             <div className="slide-container">
@@ -108,8 +139,7 @@ export function HomePage() {
             </div>
 
             <div className="main-home">
-                <h2>Find the right <span>freelance</span></h2>
-                <h2><span>service</span>, right away</h2>
+                <h2>Find the right <span>freelance service</span>, right away</h2>
                 <SearchBox placeholder={'Search for any service...'} />
                 <section className="popular-btns">Popular:
                     <button onClick={() => handleCategoryFilter('Graphic & Design')}>Graphic & Design</button>
@@ -121,8 +151,8 @@ export function HomePage() {
 
             <div className="popular-services-container">
                 <h1>Popular services</h1>
-                <button onClick={() => slideCarousel(-1)}><img src="https://www.svgrepo.com/show/350276/chevron-left.svg" alt="left" /></button>
-                <ul className="popular-services">
+                <button className='arrow' onClick={() => slideCarousel(-1)}><img src="https://www.svgrepo.com/show/350276/chevron-left.svg" alt="left" /></button>
+                <ul className="popular-services" onTouchStart={handleSwipe}>
                     {popularServices.map((service, idx) =>
                         <li key={idx} style={carouselStyle}
                             onClick={() => handleCategoryFilter(service.title)}>
@@ -132,7 +162,7 @@ export function HomePage() {
                         </li>
                     )}
                 </ul>
-                <button onClick={() => slideCarousel(1)}><img src="https://www.svgrepo.com/show/350274/chevron-right.svg" alt="right" /></button>
+                <button className='arrow' onClick={() => slideCarousel(1)}><img src="https://www.svgrepo.com/show/350274/chevron-right.svg" alt="right" /></button>
             </div>
 
             <div className="best-part-everything-container main-layout full">
